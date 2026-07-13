b854929d2380a8c5a9a9261ca9d8f3ed24d8a8e0
Giuseppe Barchetta <giuseppebarchetta8@gmail.com>
2026-07-13 14:44:41 +0200

    fix: Update permissions in validate.yml and correct import in config_flow.py

--- custom_components/fastweb_power_control/config_flow.py
+++ custom_components/fastweb_power_control/config_flow.py
@@ -7,7 +7,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import ConfigFlowResult
+from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.helpers.selector import (
     NumberSelector,
     NumberSelectorConfig,
